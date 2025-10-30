026bdc33420aa790830ab75841f668023b0e565a
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-10-30 17:01:14 +0100

    feat:login/logout functionality fixed

--- access_system/urls.py
+++ access_system/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('devices/', include('devices.urls')),
     path('access_logs/', include('access_logs.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
 ]
