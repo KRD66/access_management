f0b87d24c097a13af4ddb0ed263c04ec2be67a30
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-10-31 08:41:35 +0100

    voice print model added for voice recognition

--- users/models.py
+++ users/models.py
@@ -4,9 +4,12 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     fingerprint_id = models.CharField(max_length=100, blank=True, null=True)
-    voice_phrase = models.CharField(max_length=200, blank=True, null=True)  # ← NEW
+    voice_phrase = models.CharField(max_length=200, blank=True, null=True) 
     role = models.CharField(max_length=50, choices=[('admin', 'Admin'), ('user', 'User')], default='user')
     created_at = models.DateTimeField(auto_now_add=True)
+    voiceprint_file = models.FileField(upload_to='voiceprints/', blank=True, null=True) 
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
+    
+    
