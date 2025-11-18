8843a5bcc762c27b2ab994bfb4502e09ae0531ce
Farouq Taiwo <Farouqtaiwo45@gmail.com>
2025-11-18 18:12:59 +0100

    Fix root URL – redirect to login

--- access_system/urls.py
+++ access_system/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 from devices.views import voice_test
+from django.views.generic import RedirectView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
@@ -13,6 +14,8 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     path('voice-test/', voice_test, name='voice_test'),
     
+    path('voice-test/', include('devices.urls')),
+    path('', RedirectView.as_view(url='/login/', permanent=False)),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
